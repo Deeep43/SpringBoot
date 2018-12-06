63edc24034ce9a78a04f7d99e32ed3129e879029
Deeep43 <sheogandeepak@gmail.com>
2018-12-06 23:01:41 +0530

    test

--- springboot/src/main/resources/com/main/Example.java
+++ springboot/src/main/resources/com/main/Example.java
@@ -1,6 +1,9 @@
 package com.main;
 
+import java.awt.SystemColor;
 import java.util.Random;
 
 public class Example {
-Random r = new RandomI)();}
+Random r = new Random();
+system.out.println("deepak");
+}
